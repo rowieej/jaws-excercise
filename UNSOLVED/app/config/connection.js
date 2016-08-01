3d53c997dec40c17486325195adb6282b12ceb63
rowieej <rowena.jaico@gmail.com>
2016-08-01 19:39:01 -0400

    fixed connection

--- UNSOLVED/app/config/connection.js
+++ UNSOLVED/app/config/connection.js
@@ -9,23 +9,22 @@ var source = {
 
     localhost: {
         port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: "Bktre05220210",
-        database: 'x034u3g38h7ie3l3'
+        host: 'q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'ynttvv4lm3gs0r7x',
+        password: 'wnj3zxe21h3ijksl',
+        database: "x034u3g38h7ie3l3"
     },
     jawsDB: {
-        localhost: {
         port: 3306,
         host: 'q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
         user: 'ynttvv4lm3gs0r7x',
         password: "wnj3zxe21h3ijksl",
-        database: "starwars"
+        database: "x034u3g38h7ie3l3"
 
 
         // TODO: Add your JawsDB connection info here
-    }
-}
+        }
+};
 
 
 // TODO: connect your jawsDB connection witht he mysql connection.
